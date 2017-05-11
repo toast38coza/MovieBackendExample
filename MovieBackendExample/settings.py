6e36c0b53c9e2bb885f6a1f8b73d41b32e62aac7
toast38coza <info@38.co.za>
2017-05-11 14:17:13 +0200

    Rename to MovieBackend

--- MovieBackendExample/settings.py
+++ MovieBackendExample/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'BlogBackendExample.urls'
+ROOT_URLCONF = 'MovieBackendExample.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'BlogBackendExample.wsgi.application'
+WSGI_APPLICATION = 'MovieBackendExample.wsgi.application'
 
 
 # Database
--- MovieBackendExample/wsgi.py
+++ MovieBackendExample/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BlogBackendExample.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MovieBackendExample.settings")
 
 application = get_wsgi_application()
